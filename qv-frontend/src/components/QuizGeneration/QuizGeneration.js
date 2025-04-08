b9099e59fd4d37e223fd368f6891dafa7d4f9cda
SoniaShyjuSectionE <soniatessyshyju@gmail.com>
2025-04-08 13:49:50 -0400

    Home page , selection page

--- qv-frontend/src/components/QuizGeneration/QuizGeneration.js
+++ qv-frontend/src/components/QuizGeneration/QuizGeneration.js
@@ -3,6 +3,7 @@ import {useState, useEffect} from "react";
 import {useNavigate} from 'react-router-dom';
 import "./QuizGeneration.css";
 import axios from "axios";
+import quiz1 from '../home/Quiz1.png'
 
 const QuizGeneration = () => {
 
@@ -28,43 +29,44 @@ const QuizGeneration = () => {
     };
 
     return (
-        <>
-            <div className="QuizGeneration">
-                <h2>Customize your Quiz</h2>
+        <div className="QuizGeneration">
+            <h2>Customize your Quiz</h2>
 
-                <div className="customize-container">
-                    <label>Difficulty:</label>
-                    <select value={difficulty} className="dropdown"  onChange={(e) => setDifficulty(e.target.value)}>
-                        <option value="">-- Any Difficulty --</option>
+            <div className="customize-container">
+                <div className="customize-containerLeft">
+                    <label>Difficulty</label>
+                    <select value={difficulty} className="dropdown" onChange={(e) => setDifficulty(e.target.value)}>
+                        <option value="">Any Difficulty</option>
                         <option value="easy">Easy</option>
                         <option value="medium">Medium</option>
                         <option value="hard">Hard</option>
                     </select>
-                </div>
 
-                <div className="customize-container">
-                    <label>Type:</label>
+                    <label>Type</label>
                     <select value={type} className="dropdown" onChange={(e) => setType(e.target.value)}>
-                        <option value="">-- Any Type --</option>
+                        <option value="">Any Type</option>
                         <option value="multiple">Multiple Choice</option>
                         <option value="boolean">True / False</option>
                     </select>
-                </div>
 
-                <div className="customize-container">
-                    <label>Number of Questions:</label>
+                    <label>Number of Questions</label>
                     <input
                         type="number"
                         value={numQuestions}
                         onChange={(e) => setNumQuestions(e.target.value)}
-                        placeholder="Enter number (e.g., 5)"
+                        placeholder="Enter number below 20"
                         min="1"
-                        max="30"
+                        max="20"
                     />
+
+                    <button onClick={handleSubmit}>Start Quiz</button>
                 </div>
-                <button onClick={handleSubmit}>Start Quiz</button>
+
+                <div className="customize-containerRight">
+                    <img src={quiz1} alt="Quiz"/>
+                </div>
             </div>
-        </>
+        </div>
     );
 
 };
