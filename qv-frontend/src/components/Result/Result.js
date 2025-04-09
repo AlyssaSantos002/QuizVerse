7a2a1c552e31891d6506070f5a70930fd4e8f935
SoniaShyjuSectionE <soniatessyshyju@gmail.com>
2025-04-09 14:07:59 -0400

    quiz page, result page

--- qv-frontend/src/components/Result/Result.js
+++ qv-frontend/src/components/Result/Result.js
@@ -1,5 +1,4 @@
-import React, {useEffect} from "react";
-import {useState} from "react";
+import React, from "react";
 import {useNavigate, useLocation} from 'react-router-dom';
 import "./Result.css";
 
