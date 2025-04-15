609c0c0b72a3db19d7f56e5cd2338948992f7fb3
AlyssaSantos002 <aj.csantos1602@gmail.com>
2025-04-15 00:08:11 -0400

    Updated navbar, QuizGeneration.css, QuizGeneration.js

--- qv-frontend/src/components/NavbarComponent.js
+++ qv-frontend/src/components/NavbarComponent.js
@@ -2,27 +2,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link, useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 
-export default function NavbarComponent({ isLoggedIn, setIsLoggedIn, setUserData }) {
+export default function NavbarComponent({isLoggedIn, setIsLoggedIn, setUserData}) {
     const navigate = useNavigate();
 
     const handleLogout = async () => {
         try {
             //call logout api
-            await axios.post("/api/auth/logout", {}, { withCredentials: true });
+            await axios.post("/api/auth/logout", {}, {withCredentials: true});
 
             //clear state
             setIsLoggedIn(false);
-            setUserData({ id: "", username: "", role: "", avatar: ""});
+            setUserData({id: "", username: "", role: "", avatar: ""});
 
             //clear localStorage
             localStorage.removeItem("isLoggedIn");
             localStorage.removeItem("userData");
             //navigate to login page when logged out
             navigate("/login");
-          
+
         } catch (err) {
             console.error("Logout error:", err);
             alert("Failed to logout.");
@@ -30,6 +30,31 @@ export default function NavbarComponent({ isLoggedIn, setIsLoggedIn, setUserData
     };
 
     return (
+        // <>
+        //     <Navbar bg="transparent" data-bs-theme="dark">
+        //         <Container>
+        //             <Navbar.Brand href="#home">QuizVerse</Navbar.Brand>
+        //             <Nav className="me-auto">
+        //                 <Nav.Link as={Link} to="/">Home</Nav.Link>
+        //                 <Nav.Link as={Link} to="/">Search</Nav.Link>
+        //             </Nav>
+        //             <Nav className="ms-auto">
+        //                 {isLoggedIn ? (
+        //                     <>
+        //                         <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+        //                         <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
+        //                     </>
+        //                 ) : (
+        //                     <>
+        //                         <Nav.Link as={Link} to="/login">Login</Nav.Link>
+        //                         <Nav.Link as={Link} to="/register">Register</Nav.Link>
+        //                     </>
+        //                 )}
+        //             </Nav>
+        //         </Container>
+        //     </Navbar>
+        // </>
+
         <Navbar expand="lg" className="bg-body-transparent" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand as={Link} to="/">QuizVerse</Navbar.Brand>
